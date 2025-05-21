ae0f13061fff1be56957ee045666682517a8c2e2
lemmyszn <lemmyxking17@gmail.com>
2025-05-21 09:41:36 +0300

    Add sum function with test cases

---
+++ sum.js
@@ -0,0 +1,14 @@
+// Function to add two numbers
+function sum(num1, num2) {
+    return num1 + num2;
+}
+
+// Test the function with some numbers
+const result1 = sum(5, 3);
+console.log("Sum of 5 and 3 is:", result1);
+
+const result2 = sum(10, 20);
+console.log("Sum of 10 and 20 is:", result2);
+
+// You can also use the function directly in console.log
+console.log("Sum of 7 and 8 is:", sum(7, 8)); 
